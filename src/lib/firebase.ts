c529e0a4dd4e820b512b3a9b70e9c5135d6fbb16
ocomstudio <ocomstudio@gmail.com>
2025-08-12 09:56:38 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDwNEwdzJpreyYhL33McDIoSsXxg62Y

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 import { initializeFirestore, CACHE_SIZE_UNLIMITED, enableIndexedDbPersistence } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDwNEwdzJpreyYhL33McDIoSsXxg62Y6KU",
+  authDomain: "wisebil-a1757.firebaseapp.com",
+  databaseURL: "https://wisebil-default-rtdb.firebaseio.com",
+  projectId: "wisebil",
+  storageBucket: "wisebil.firebasestorage.app",
+  messagingSenderId: "863943790691",
+  appId: "1:863943790691:web:0ee38b99a42651f099a856"
 };
 
 // Initialize Firebase
