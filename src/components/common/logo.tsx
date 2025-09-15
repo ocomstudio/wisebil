9ab162b7aa4fabdd5340cefa32554a5b28302169
ocomstudio <ocomstudio@gmail.com>
2025-09-15 12:15:41 +0000

    Nous allons faire une mise à jour très importance sur l’application. Nou

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -1,18 +1,19 @@
-import { Leaf } from 'lucide-react';
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export function Logo({ className, isLink = false }: { className?: string; isLink?: boolean }) {
   const content = (
-      <div className={cn('flex items-center gap-2', className)}>
-        <div className="bg-primary p-1.5 rounded-sm">
-          <Leaf className="h-5 w-5 text-white" />
-        </div>
-        <span className="text-xl font-bold font-headline">
-          <span className="text-foreground">Wise</span>
-          <span className="text-primary">bil</span>
-        </span>
-      </div>
+    <div className={cn('flex items-center', className)}>
+      <Image
+        src="/logo.png"
+        alt="Wisebil Logo"
+        width={130}
+        height={32}
+        className="object-contain"
+        priority
+      />
+    </div>
   );
 
   if (isLink) {
